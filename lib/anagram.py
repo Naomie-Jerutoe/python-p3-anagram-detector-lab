5794675c5646211e5819408080055da0c7069293
Naomi Lagat <naomiejerutoe@gmail.com>
2024-03-01 12:14:37 +0300

    completed task

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word_letters = sorted([letter for letter in word])
+
+  def match(self, anagram_list):
+    match_anagram_list = []
+
+    for word in anagram_list:
+      if sorted([letter for letter in word]) == self.word_letters:
+        match_anagram_list.append(word)
+    
+    return match_anagram_list
